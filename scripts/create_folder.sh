d8f6eb1b273f40e23d618e69c5b6b503dc318685
Floren-teena <florentinaantigha27@gmail.com>
2025-02-27 00:19:00 +0100

    Added .gitkeep files to track empty odd-numbered folders

--- scripts/create_folder.sh
+++ scripts/create_folder.sh
@@ -7,10 +7,14 @@ for i in {1..5}; do
   # Create the folder
   mkdir -p "$folder_name"
   
-  # Check if the folder number is even
+  # Check if the folder number is even or odd
   if (( i % 2 == 0 )); then
     # Create alpha.txt and beta.txt in even-numbered folders
     touch "$folder_name/alpha.txt" "$folder_name/beta.txt"
+  fi
+  else
+    # Add a .gitkeep file to empty odd-numbered folders
+    touch "$folder_name/.gitkeep"
   fi
 done
 
